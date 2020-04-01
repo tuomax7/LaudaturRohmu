b40c35b4fbe56b7d6f959ff0a917c0a05fb6cabf
Tuomas Nummela <tuomasnummela@Tuomas-MBP-2.lan>
2020-04-01 14:52:20 +0300

    Alustettu kori ja pallojen runko (luodaan ensin pelin mekaniikka, lisätään visuaalisuus myöhemmin arvosanoina ja yo-lakkina.

---
+++ ball.js
@@ -0,0 +1,10 @@
+
+
+function Ball(x, y, s, c, v){
+
+	this.pos = createVector(x, y);
+	this.velocity = createVector(0, v);
+	this.size = s;
+	this. color = c;
+
+}
--- catcher.js
+++ catcher.js
@@ -1,10 +1,34 @@
+var basket;
+const B_SIZE = 50;
+const HALF_B_SIZE = B_SIZE / 2;
 
 function setup(){
 	createCanvas(400, 600);
+
+	basket = createVector(width / 2, height - B_SIZE);
 
 }
 
 function draw(){
 
 	background(51);
+
+	handleBasket();
+}
+
+
+function handleBasket(){
+	basket.x = constrain(mouseX, 0, width);
+
+	stroke(255);
+	strokeWeight(3);
+	noFill();
+
+	beginShape();
+	vertex(basket.x - HALF_B_SIZE, basket.y - HALF_B_SIZE);
+	vertex(basket.x - HALF_B_SIZE, basket.y + HALF_B_SIZE);
+	vertex(basket.x + HALF_B_SIZE, basket.y + HALF_B_SIZE);
+	vertex(basket.x + HALF_B_SIZE, basket.y - HALF_B_SIZE);
+	endShape();
+
 }
